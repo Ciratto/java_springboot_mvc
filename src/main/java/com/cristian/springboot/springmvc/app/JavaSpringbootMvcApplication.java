8ed968a3f8773ff05ae29c9f482e3648f7c5f70e
Ciratto <cris81ar@gmail.com>
2026-08-14 01:36:31 -0300

    Proyecto iniciado con Spring Initializr

---
+++ src/main/java/com/cristian/springboot/springmvc/app/JavaSpringbootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cristian.springboot.springmvc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristian/springboot/springmvc/app/java_springboot_mvc/JavaSpringbootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.springboot.springmvc.app.java_springboot_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
