b4775bad2f9180e60c02be56149fa42a9d99db80
Ciratto <cris81ar@gmail.com>
2026-08-21 00:50:16 -0300

    Primer Hola Mundo

---
+++ src/main/java/com/cristian/springboot/springmvc/app/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.cristian.springboot.springmvc.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app")
+public class UserController {
+
+    @GetMapping("view")
+    public String obtenerDatosVista(Model modelo) {
+    	modelo.addAttribute("titulo", "Hola Spring Boot MVC");
+    	modelo.addAttribute("mensaje", "Esto es un mensaje desde el controlador");
+        return "viewModel";
+    }
+}
